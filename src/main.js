65e0df5b43b1c2b8c664fa5587d31f616ef4a858
uulwake <uulwake@example.com>
2020-02-19 16:45:25 +0700

    Nav Guard: Done

--- src/main.js
+++ src/main.js
@@ -7,6 +7,15 @@ import store from './store/index';
 
 Vue.config.productionTip = false;
 
+router.beforeEach((to, from, next) => {
+  if (to.name !== from.name && to.name !== 'Register' && to.name !== 'Login') {
+    if (!localStorage.access_token) next('/');
+    else next();
+  } else {
+    next();
+  }
+});
+
 new Vue({
   router,
   store,
