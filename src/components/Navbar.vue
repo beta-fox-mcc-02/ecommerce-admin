aadaf5af06185842218cbec4ab654ffba9cfa543
mcc-14 <mcc-14@mcc-14s-Mac-mini.local>
2020-02-19 16:45:01 +0700

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                <router-link to="/">Home</router-link>
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                <router-link to="/about">About</router-link>
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>
+                            <router-link to="/register">Register</router-link>
+                        </strong>
+                    </a>
+                    <a class="button is-light">
+                        <router-link to="/login">Login</router-link>
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+div {
+  background-image: url('https://i.pinimg.com/originals/eb/09/85/eb098576a0c618c99ee86406a8815c64.png');
+  background-size: contain
+}
+</style>
