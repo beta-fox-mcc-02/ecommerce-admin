867130db0f907dac4c4a8c2ca9b732387d45aafb
nafies1 <ghuroba.muslim@gmail.com>
2020-02-20 13:54:05 +0700

    on working, Sign page

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isLoginPage: true,
+    loadingForm: false
   },
   mutations: {
+    changeSignForm (state, status) {
+      console.log('mutation kepencet')
+      state.isLoginPage = status
+    },
+    runLoading (state, status) {
+      state.loadingForm = status
+    }
   },
   actions: {
   },
