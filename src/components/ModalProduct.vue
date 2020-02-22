53a2380fcf1fefc7c7a4b214efa7d2d07b3235bf
baufakhran <baufakhran@gmail.com>
2020-02-22 17:43:35 +0700

    fix minor bug

--- src/components/ModalProduct.vue
+++ src/components/ModalProduct.vue
@@ -78,6 +78,7 @@ export default {
           } else if (err.response.data.errors) {
             this.error = err.response.data.errors[0]
           }
+          this.loading = false
         })
     }
   }
